0d536cd4d2ad3133b8add64adf61243384402a7a
Huda Nassar <hnassar@purdue.edu>
2015-06-14 19:58:48 -0400

    experiment

---
+++ pagerank_solver.jl
@@ -0,0 +1,33 @@
+function pagerank_solver(A,k,alpha)
+# k is the number of vectors under the experiment
+
+# Generate transition matrix P
+n = size(A,1);
+s = sum(A,2);
+idx = 1:n;
+vals = 1./s;
+P = sparse(idx, idx, vals[:])*A;
+
+## Create the vectors V
+
+p = sortperm(s[:],rev=true);
+ids = p[1:k];
+
+# set up our vectors v:
+V = zeros(Float64,n,k);
+for v = 1:k
+    V[ids[v],v] = 1;
+end
+
+## Solve the PageRank problem using the power method
+
+X = copy(V);
+err = 1e-12;
+iters_nb = floor((log(err/2))/(log(alpha)));
+for i = 1:iters_nb
+    X = alpha*(P'*X) + (1-alpha)*V;
+end
+X_exact = X;
+return X;
+end
+
