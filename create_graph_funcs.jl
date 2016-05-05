28265854921a22780677b1f512b9b5c72aa724c9
Huda Nassar <hnassar@purdue.edu>
2016-05-05 14:17:35 -0400

    updated graph generation with option to save src and dst

--- create_graph_funcs.jl
+++ create_graph_funcs.jl
@@ -4,31 +4,30 @@ else
     const libpath = string(pwd(), "/libbisquik.dylib")
 end
 
-include("degseq.jl");
+include("degseq.jl")
 function bisquik_graph(degs::Array{Int64},trials::Int64,n::Int64)
-	trial_number = 1;
-    @printf("trial number: %i\n",trial_number);
+    trial_number = 1
+    @printf("trial number: %i\n",trial_number)
     check_flag = ccall ( (:check_graphical_sequence, libpath),
                         Cint, # return value
                         (Int64, Ptr{Int64}), # arg types
                         n, degs); # actual args
-    trial_number = 2;
-	while trial_number <= trials && check_flag == 0
-		@printf("trial number: %i\n",trial_number);
-        #degs = convert(Array{Int64},degs + floor(0.001*degs.*rand(length(degs),1)));
+    trial_number = 2
+    while trial_number <= trials && check_flag == 0
+        @printf("trial number: %i\n",trial_number)
         degs[end] += 1
         if mod(sum(degs),2) != 0
-        	degs[end] = degs[end]+1;
+            degs[end] = degs[end]+1
         end
         check_flag = ccall ( (:check_graphical_sequence, libpath),
                         Cint, # return value
                         (Int64, Ptr{Int64}), # arg types
-                        n, degs); # actual args
-		trial_number = trial_number + 1;
+                        n, degs) # actual args
+        trial_number = trial_number + 1
 	end
-
-    if check_flag == 1
-		@printf("Successfully generated a graphical sequence.\n")
+	
+	if check_flag == 1
+	    @printf("Successfully generated a graphical sequence.\n")
         nedges = sum(degs)
         src = zeros(Int64, nedges)
         dst = zeros(Int64, nedges)
@@ -36,9 +35,9 @@ function bisquik_graph(degs::Array{Int64},trials::Int64,n::Int64)
                         Cint, # return value
                         (Int64, Ptr{Int64}, Ptr{Int64}, Ptr{Int64}), # arg types
                         length(degs), degs, src, dst) # actual args
-		src = src+1;
-    	dst = dst+1;
-    	A = sparse(src, dst, 1, length(degs), length(degs));
+        src = src+1
+        dst = dst+1
+        @printf("Successfully generated graph.\n")
     else
    		@printf("COULD NOT GENERATE A GRAPHICAL SEQUENCE\n")
 		###############################
@@ -47,59 +46,38 @@ function bisquik_graph(degs::Array{Int64},trials::Int64,n::Int64)
 		assert(false);
     end
 
-    return A;
+    l = length(degs)
+    return (src,dst,l)
 end
 
 function check_graphical_sequence(degs::Array{Int64},trials::Int64,n::Int64)
-	trial_number = 1;
-    @printf("trial number: %i\n",trial_number);
+    trial_number = 1
+    @printf("trial number: %i\n",trial_number)
     check_flag = ccall ( (:check_graphical_sequence, libpath),
                         Cint, # return value
                         (Int64, Ptr{Int64}), # arg types
-                        n, degs); # actual args
-    trial_number = 2;
-	while trial_number <= trials && check_flag == 0
-		@printf("trial number: %i\n",trial_number);
-        #degs = convert(Array{Int64},degs + floor(0.001*degs.*rand(length(degs),1)));
+                        n, degs) # actual args
+    trial_number = 2
+    while trial_number <= trials && check_flag == 0
+        @printf("trial number: %i\n",trial_number)
         degs[end] += 1
         if mod(sum(degs),2) != 0
-        	degs[end] = degs[end]+1;
+            degs[end] = degs[end]+1
         end
         check_flag = ccall ( (:check_graphical_sequence, libpath),
                         Cint, # return value
                         (Int64, Ptr{Int64}), # arg types
-                        n, degs); # actual args
-		trial_number = trial_number + 1;
-	end
-	return (degs,check_flag)
-end
-
-function bisquik_graph2(degs::Array{Int64},n::Int64)
-
-    #@printf("Successfully generated a graphical sequence.\n")
-    nedges = sum(degs)
-    src = zeros(Int64, nedges)
-    dst = zeros(Int64, nedges)
-    rval = ccall ( (:generate_bisquik_graph, libpath),
-                        Cint, # return value
-                        (Int64, Ptr{Int64}, Ptr{Int64}, Ptr{Int64}), # arg types
-                        length(degs), degs, src, dst) # actual args
-    src = src+1
-    dst = dst+1
-    return (src,dst)
+                        n, degs) # actual args
+        trial_number = trial_number + 1
+    end
+    return (degs,check_flag)
 end
 
 
 function create_graph(p::Float64,n::Int64,dmax::Int64,dmin::Int64)
-	n = convert(Int64,n)
+	n = round(Int,n)
    	trials = 5
 	degs_vector = convert(Array{Int64,1},ceil(degseq(p,n,dmax,dmin)))
-	A = bisquik_graph(degs_vector,trials,n)
-	return A
-end
-
-# if we just want to look at src and dst
-function create_graph(degs_vector::Vector{Int64},n::Int64)
-	(src,dst) = bisquik_graph2(degs_vector,n)
-	return (src,dst)
+	(src,dst,l) = bisquik_graph(degs_vector,trials,n)
+	return (src,dst,l)
 end
--- degseq.jl
+++ degseq.jl
@@ -1,10 +1,10 @@
 function degseq(p::Float64,n::Int64,dmax::Int64,dmin::Int64)
-	last = floor((dmin/dmax)^(-1/p));
-	v = ones(Int64,n);
-	v = v.*dmin;
-
-	for k = 1:last
-		v[k] = ceil(dmax/(k^p));
-	end
-	return v;
+    last = round(Int,floor((dmin/dmax)^(-1/p)))
+    v = ones(Int64,n)
+    v = v.*dmin
+    
+    for k = 1:last
+        v[k] = ceil(dmax/(k^p))
+    end
+    return v
 end
